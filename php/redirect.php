a697cb54b0df92fbb50c87620da8eb48dc78ef85
UneMeilleureSante <56452873+UneMeilleureSante@users.noreply.github.com>
2019-10-11 22:29:02 +0300

    Add files via upload

---
+++ php/redirect.php
@@ -0,0 +1,22 @@
+<?php
+
+$redirect = array(
+	
+	// Ссылка на сайт накоторый нужно перейти залогиненному пользователю
+	// "grayUrl" => "http://bit.ly/KetoOfficial",
+		"grayUrl" => "http://bit.ly/KetoOfficial",
+	// http://nubagu.ffftgfr.com/tracker/debug.php?param_three=val3&param_four=val4		
+		
+	// Название папки с серым лендингом
+	//"grayFolder" => "innerFolder",
+	//"grayFolder" => "grayLand",
+	"grayFolder" => "blackLand",
+	
+	// Нужно перейти на адрес прописанный выше
+	// "isRedirect" => true,
+	
+	// Нужно перейти в каталог innerFolder
+	"isRedirect" => true,
+	
+	
+);
